92d8b674ef8c47d8d3383489ca2f58022427031c
DeedleFake <deedlefake@users.noreply.github.com>
2019-05-20 14:10:59 -0400

    Updates

---
+++ precache-manifest.21baeff55d6e36504a80a09900089c8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60dee1e861ac1260bfcc884fd729e7c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc2a3a6bbec5401949b9",
+    "url": "/static/css/main.1ab02496.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "bc2a3a6bbec5401949b9",
+    "url": "/static/js/main.844bd797.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
