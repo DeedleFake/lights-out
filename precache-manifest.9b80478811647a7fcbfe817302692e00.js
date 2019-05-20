3f297c1756ece020bf79b3508d8c6326b52b4076
DeedleFake <deedlefake@users.noreply.github.com>
2019-05-20 14:19:52 -0400

    Updates

---
+++ precache-manifest.9b80478811647a7fcbfe817302692e00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc76c9666b83e87606e0648dfa5d0908",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "bc2a3a6bbec5401949b9",
+    "url": "/lights-out/static/css/main.1ab02496.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/lights-out/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "bc2a3a6bbec5401949b9",
+    "url": "/lights-out/static/js/main.844bd797.chunk.js"
+  },
+  {
+    "revision": "d6b7daafe64bb82a9237",
+    "url": "/lights-out/static/js/runtime~main.69e4817a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7be424d77c05e2df4a5eb9ee2099b2e6.js"
+  "/lights-out/precache-manifest.9b80478811647a7fcbfe817302692e00.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lights-out/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
