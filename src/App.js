b940ed6d2d29c642828e45e6872236f88bb48274
DeedleFake <deedlefake@users.noreply.github.com>
2019-05-20 14:04:53 -0400

    Some more polish.

--- src/App.js
+++ src/App.js
@@ -37,66 +37,85 @@ const App = () => {
 	)
 
 	return (
-		<div
-			style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-		>
-			<h2>Lights Out</h2>
+		<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+			<div style={{ display: 'flex', flexDirection: 'column', width: 500 }}>
+				<h1>Lights Out</h1>
 
-			<svg
-				xmlns="https://www.w3.org/2000/svg"
-				style={{ width: 300, marginTop: 16, border: `solid 16px ${won ? 'green' : 'grey'}`, borderRadius: 32 }}
-				viewBox={`0 0 ${10 * board[0].length} ${10 * board.length}`}
-			>
-				{board.map((row, y) =>
-					row.map((space, x) => (
-						<circle
-							key={x}
-							style={{
-								cursor: 'pointer',
-							}}
-							cx={10 * x + 5}
-							cy={10 * y + 5}
-							r={4}
-							fill={space ? 'lightblue' : 'darkblue'}
-							onClick={(ev) => {
-								ev.preventDefault()
-								dispatchBoard({ type: 'toggle', space: [x, y] })
-							}}
-						/>
-					)),
-				)}
-			</svg>
-
-			<div style={{ display: 'flex', flexDirection: 'row', marginTop: 16 }}>
-				<input
+				<svg
+					xmlns="https://www.w3.org/2000/svg"
 					style={{
-						fontSize: 20,
+						marginTop: 16,
+						border: `solid 16px ${won ? 'green' : 'grey'}`,
+						borderRadius: 32,
 					}}
-					type="number"
-					min="2"
-					max="10"
-					value={size}
-					onChange={(ev) => {
-						let val = parseInt(ev.target.value, 10)
-						setSize((size) => isNaN(val) || val < 2 || val > 10 ? size : val)
-					}}
-				/>
-
-				<button
-					style={{
-						backgroundColor: 'cyan',
-						borderRadius: 8,
-						fontSize: 20,
-						padding: 8,
-						marginLeft: 8,
-					}}
-					onClick={() => dispatchBoard({ type: 'reset', size })}
+					viewBox={`0 0 ${10 * board[0].length} ${10 * board.length}`}
 				>
-					{size !== board.length ? 'Resize' : 'Reset'}
-				</button>
-			</div>
+					{board.map((row, y) =>
+						row.map((space, x) => (
+							<rect
+								key={x}
+								style={{
+									transition: 'fill .3s',
+									cursor: won || 'pointer',
+								}}
+								x={10 * x + 1}
+								y={10 * y + 1}
+								width={8}
+								height={8}
+								rx={1}
+								fill={space ? '#3333FF' : '#000099'}
+								onClick={(ev) => {
+									ev.preventDefault()
+									if (won) {
+										return
+									}
 
-			{won && <h2 style={{ marginTop: 16 }}>Congratulations</h2>}
+									dispatchBoard({ type: 'toggle', space: [x, y] })
+								}}
+							/>
+						)),
+					)}
+				</svg>
+
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+						marginTop: 16,
+							padding: '0 32px',
+					}}
+				>
+					<input
+						style={{
+							fontSize: 20,
+						}}
+						type="number"
+						min="2"
+						max="10"
+						value={size}
+						onChange={(ev) => {
+							let val = parseInt(ev.target.value, 10)
+							setSize((size) =>
+								isNaN(val) || val < 2 || val > 10 ? size : val,
+							)
+						}}
+					/>
+
+					<button
+						style={{
+							backgroundColor: 'cyan',
+							borderRadius: 8,
+							fontSize: 20,
+							padding: 8,
+							marginLeft: 8,
+						}}
+						onClick={() => dispatchBoard({ type: 'reset', size })}
+					>
+						{size !== board.length ? 'Resize' : 'Reset'}
+					</button>
+				</div>
+			</div>
 		</div>
 	)
 }
