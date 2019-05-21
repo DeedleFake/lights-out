7342d8b282caf57dc5ea1e85795882d7269cbb7b
DeedleFake <deedlefake@users.noreply.github.com>
2019-05-21 13:27:43 -0400

    Implement Othello instead.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,117 @@
 // @format
 
-import React, { useReducer, useMemo, useState } from 'react'
+import React, { useReducer, useMemo, useState, useCallback } from 'react'
 
 import './style.css'
 
 const App = () => {
-	const [size, setSize] = useState(5)
+	const [turn, setTurn] = useState(false)
 
 	const [board, dispatchBoard] = useReducer(
 		(board, action) => {
 			switch (action.type) {
-				case 'toggle':
+				case 'place':
 					return board.map((row, y) =>
 						row.map((space, x) =>
-							(Math.abs(action.space[0] - x) <= 1 && action.space[1] === y) ||
-							(Math.abs(action.space[1] - y) <= 1 && action.space[0] === x)
-								? !space
+							x === action.space[0] && y === action.space[1]
+								? action.piece
 								: space,
 						),
 					)
 
 				case 'reset':
-					return new Array(action.size).fill(new Array(action.size).fill(true))
+					return board.map((row, y) =>
+						row.map((space, x) =>
+							x >= row.length / 2 - 1 &&
+							x <= row.length / 2 &&
+							y >= board.length / 2 - 1 &&
+							y <= board.length / 2
+								? (x + y) % 2 === 0
+								: null,
+						),
+					)
 
 				default:
 					throw new Error(`Unknown action type: ${action.type}`)
 			}
 		},
-		size,
-		(size) => new Array(size).fill(new Array(size).fill(true)),
+		8,
+		(size) => {
+			let a = new Array(size).fill(null).map(() => new Array(size).fill(null))
+			for (let y = a.length / 2 - 1; y <= a.length / 2; y++) {
+				a[y][a[y].length / 2 - 1] = y % 2 !== 0
+				a[y][a[y].length / 2] = y % 2 === 0
+			}
+			return a
+		},
 	)
 
-	const won = useMemo(
-		() => board.every((row) => row.every((space) => !space)),
+	const score = useMemo(
+		() =>
+			board.reduce(
+				(score, row) =>
+					row.reduce(
+						(score, space) => score + (space == null ? 0 : space ? 1 : -1),
+						score,
+					),
+				0,
+			),
+		[board],
+	)
+
+	const toBeFlipped = useCallback(
+		([sx, sy], [dx, dy], piece) => {
+			const [dsx, dsy] = [
+				dx > 0 ? 1 : dx < 0 ? -1 : 0,
+				dy > 0 ? 1 : dy < 0 ? -1 : 0,
+			]
+
+			let flip = []
+			for (
+				let [x, y] = [sx + dsx, sy + dsy];
+				y >= 0 &&
+				y < board.length &&
+				x >= 0 &&
+				x < board[y].length &&
+				board[y][x] != null;
+				[x, y] = [x + dx, y + dy]
+			) {
+				if (board[y][x] === piece) {
+					return flip
+				}
+
+				flip.push([x, y])
+			}
+
+			return []
+		},
+		[board],
+	)
+
+	const placePiece = useCallback(
+		(space, piece) => {
+			dispatchBoard({ type: 'place', space, piece })
+
+			for (let dy = -1; dy <= 1; dy++) {
+				for (let dx = -1; dx <= 1; dx++) {
+					if (dx === 0 && dy === 0) {
+						continue
+					}
+
+					for (let s of toBeFlipped(space, [dx, dy], piece)) {
+						dispatchBoard({ type: 'place', space: s, piece })
+					}
+				}
+			}
+		},
+		[toBeFlipped],
+	)
+
+	const isLegal = useCallback(
+		(x, y) => {
+			// TODO: Prevent moves that don't flip.
+			return board[y][x] == null
+		},
 		[board],
 	)
 
@@ -40,18 +119,18 @@ const App = () => {
 		<div
 			style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
 		>
-			<div
-				style={{ display: 'flex', flexDirection: 'column', maxWidth: 800 }}
-			>
-				<h1>Lights Out</h1>
+			<div style={{ display: 'flex', flexDirection: 'column', maxWidth: 800 }}>
+				<h1>Othello</h1>
 
 				<svg
 					xmlns="https://www.w3.org/2000/svg"
 					style={{
+						backgroundColor: 'darkgrey',
 						marginTop: 16,
-						border: `solid 16px ${won ? 'green' : 'grey'}`,
+						border: `solid 16px ${turn ? 'white' : 'black'}`,
 						borderRadius: 32,
 						padding: 8,
+						transition: 'border .3s',
 					}}
 					viewBox={`0 0 ${10 * board[0].length} ${10 * board.length}`}
 				>
@@ -61,21 +140,22 @@ const App = () => {
 								key={x}
 								style={{
 									transition: 'fill .3s',
-									cursor: won || 'pointer',
+									cursor: 'pointer',
 								}}
 								x={10 * x + 1}
 								y={10 * y + 1}
 								width={8}
 								height={8}
 								rx={1}
-								fill={space ? '#3333FF' : '#000099'}
+								fill={space == null ? 'grey' : space ? 'white' : 'black'}
 								onClick={(ev) => {
 									ev.preventDefault()
-									if (won) {
+									if (!isLegal(x, y)) {
 										return
 									}
 
-									dispatchBoard({ type: 'toggle', space: [x, y] })
+									placePiece([x, y], turn)
+									setTurn((turn) => !turn)
 								}}
 							/>
 						)),
@@ -91,22 +171,12 @@ const App = () => {
 						padding: '0 32px',
 					}}
 				>
-					<input
-						style={{
-							fontSize: 20,
-						}}
-						type="number"
-						min="2"
-						value={size}
-						onChange={(ev) => {
-							let val = ev.target.value
-							if (val.match(/^[0-9]*$/) == null) {
-								return
-							}
-
-							setSize(parseInt(val, 10))
-						}}
-					/>
+					<h3>
+						Score:{' '}
+						{score !== 0
+							? `+${Math.abs(score)} ${score > 0 ? 'White' : 'Black'}'s Favor`
+							: 'Tied'}
+					</h3>
 
 					<button
 						style={{
@@ -116,22 +186,12 @@ const App = () => {
 							padding: 8,
 							marginLeft: 8,
 						}}
-						disabled={!(size > 0)}
 						onClick={() => {
-							if (
-								size !== board.length &&
-								size > 20 &&
-								!window.confirm(
-									`Large board sizes can run pretty badly and be basically unplayable.\nAre you sure that you want to use a ${size}x${size} board?`,
-								)
-							) {
-								return
-							}
-
-							dispatchBoard({ type: 'reset', size })
+							dispatchBoard({ type: 'reset' })
+							setTurn(false)
 						}}
 					>
-						{size !== board.length ? 'Resize' : 'Reset'}
+						Reset
 					</button>
 				</div>
 			</div>
